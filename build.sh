cb7f0fc591a7789e1e41b22d330f35df4e03386e
Seidel-Michael <michael.seidel@balluff.de>
2021-05-03 21:05:44 +0200

    fix: Add --platform to build cmd

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 VERSION=1.8.5
 DOCKERHUB=docker.io/distrolessdocker/distroless-influxdb
 
-docker build --build-arg VERSION=$VERSION --build-arg ARCH=amd64 --build-arg STATIC=-static -t $DOCKERHUB:amd64-$VERSION .
-docker build --build-arg VERSION=$VERSION --build-arg ARCH=armhf --build-arg STATIC= -t $DOCKERHUB:armhf-$VERSION .
-docker build --build-arg VERSION=$VERSION --build-arg ARCH=armhf --build-arg STATIC= -t $DOCKERHUB:arm64-$VERSION .
+docker build --build-arg VERSION=$VERSION --platform linux/amd64 --build-arg ARCH=amd64 --build-arg STATIC=-static -t $DOCKERHUB:amd64-$VERSION .
+docker build --build-arg VERSION=$VERSION --platform linux/arm/v7 --build-arg ARCH=armhf --build-arg STATIC= -t $DOCKERHUB:armhf-$VERSION .
+docker build --build-arg VERSION=$VERSION --platform linux/arm64/v8 --build-arg ARCH=armhf --build-arg STATIC= -t $DOCKERHUB:arm64-$VERSION .
 docker push $DOCKERHUB:amd64-$VERSION
 docker push $DOCKERHUB:armhf-$VERSION
 docker push $DOCKERHUB:arm64-$VERSION
@@ -15,5 +15,5 @@ DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create --amend $DOCKERHUB:$VERSI
 
 DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate $DOCKERHUB:$VERSION $DOCKERHUB:amd64-$VERSION --os linux --arch amd64
 DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate $DOCKERHUB:$VERSION $DOCKERHUB:armhf-$VERSION --os linux --arch arm --variant v7
-DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate $DOCKERHUB:$VERSION $DOCKERHUB:armhf-$VERSION --os linux --arch arm64 --variant v8
+DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate $DOCKERHUB:$VERSION $DOCKERHUB:arm64-$VERSION --os linux --arch arm64 --variant v8
 DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push $DOCKERHUB:$VERSION
